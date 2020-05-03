324cddabf6f5a12dd13e00ba894395e6d3a067bb
Emir Önder Demircan <62562622+emironderdemircan@users.noreply.github.com>
2020-05-03 18:19:00 +0300

    proje klasörleri düzenlendi
    Proje klasörleri düzenlendi.
    Resimler eklendi.

--- admin.php
+++ admin.php
@@ -2,10 +2,10 @@
 include("ayar.php");
 session_start();
 if(!isset($_SESSION["login"])){
-echo "Bu sayfayı görüntüleme yetkiniz yoktur.";
+	echo "Bu sayfayı görüntüleme yetkiniz yoktur.";
 }else{
-echo "HOŞGELDİNİZ EMİR ÖNDER DEMİRCAN<br>";
-echo "ANA SAYFAYA YÖNLENDİRİLİYORSUNUZ...";
-echo header("Refresh: 3; url=anasayfa.html");
+	echo "HOŞGELDİNİZ EMİR ÖNDER DEMİRCAN<br>";
+	echo "ANA SAYFAYA YÖNLENDİRİLİYORSUNUZ...";
+	echo header("Refresh: 3; url=anasayfa.html");
 }
 ?>
--- send.php
+++ send.php
@@ -1,21 +1,21 @@
 <?php 
-    if(isset($_POST["gönder"])){
- 		$v1 = $_POST["texttypead"];
- 		$v2 = $_POST["texttypesoyad"];
- 		$v3 = $_POST["texttypeemail"];
- 		$v4 = $_POST["texttypekonu"];
- 		$v5 = $_POST["texttypemesaj"];
- 		$v6 = $_POST["texttypedosya"];
- 		echo $v1;
- 		echo "<br>";
- 		echo $v2;
- 		echo "<br>";
- 		echo $v3;
- 		echo "<br>";
- 		echo $v4;
- 		echo "<br>";
- 		echo $v5;
- 		echo "<br>";
- 		echo $v6;		
- 	}
+if(isset($_POST["gönder"])){
+	$v1 = $_POST["texttypead"];
+	$v2 = $_POST["texttypesoyad"];
+	$v3 = $_POST["texttypeemail"];
+	$v4 = $_POST["texttypekonu"];
+	$v5 = $_POST["texttypemesaj"];
+	$v6 = $_POST["texttypedosya"];
+	echo $v1;
+	echo "<br>";
+	echo $v2;
+	echo "<br>";
+	echo $v3;
+	echo "<br>";
+	echo $v4;
+	echo "<br>";
+	echo $v5;
+	echo "<br>";
+	echo $v6;		
+}
 ?>
